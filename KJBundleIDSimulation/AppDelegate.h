59dd64361aaf9620c90333ceaf7fd0764ca9eb75
TigerHu <yuanxuehu2016@126.com>
2024-08-16 15:15:20 +0800

    Initial Commit

---
+++ KJBundleIDSimulation/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  KJBundleIDSimulation
+//
+//  Created by TigerHu on 2024/8/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ KJBundleIDSimulation/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  KJBundleIDSimulation
+//
+//  Created by TigerHu on 2024/8/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
